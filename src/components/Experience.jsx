87d3a859444907fd7a570f5d6ffe860bdfbf1e33
Parth9136 <shuklaparth916@gmail.com>
2025-07-03 12:01:35 +0530

    initial commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: "Idle",
+      options: ["Idle", "Angry", "Frustrated", "Stressed", "Happy", "Sad"],
+    },
+  });
+
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1.2} scale={1.2}>
+        <Avatar animation={animation} />
+      </group>
+      <ambientLight intensity={3} />
     </>
   );
 };
